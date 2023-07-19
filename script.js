f0fac7c7b93a651f15ab1e872fdd1234cd2a70ad
lex23unwin <lex23unwin@gmail.com>
2023-07-18 19:57:45 -0500

    Add singleRound function to simulate round of RPS
    By providing the logic behind a single round of the
    game, the user will be able to play rock paper
    scissors like the rules of the game intend.

--- script.js
+++ script.js
@@ -11,3 +11,52 @@ function getComputerChoice()
     }
     return "scissors"
 }
+
+function singleRound(playerSelection, computerSelection, playerScore, computerScore)
+{
+    if (playerSelection === "rock")
+    {
+        if (computerSelection === "rock")
+        {
+            console.log(`You Tie! ${playerSelection} ties ${computerSelection}`);
+            return `You Tie! ${playerSelection} ties ${computerSelection}`;
+        }
+        else if (computerSelection === "paper")
+        {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        }
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === "paper")
+    {
+        if (computerSelection === "rock")
+        {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+        else if (computerSelection === "paper")
+        {
+            console.log(`You Tie! ${playerSelection} ties ${computerSelection}`);
+            return `You Tie! ${playerSelection} ties ${computerSelection}`;
+        }
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else /*playerSelection === "scissors"*/
+    {
+        if (computerSelection === "rock")
+        {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+            return "You Lose! ${computerSelection} beats ${playerSelection}";
+        }
+        else if (computerSelection === "paper")
+        {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        }
+        console.log(`You Tie! ${playerSelection} ties ${computerSelection}`);
+        return `You Tie! ${playerSelection} ties ${computerSelection}`;
+    }
+}
