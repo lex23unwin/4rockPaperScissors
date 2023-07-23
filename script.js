05d1dc9a643631b2f99d4180fa3f6851f0c81f2b
lex23unwin <lex23unwin@gmail.com>
2023-07-23 17:39:04 -0500

    Fixed the forEach glitch

--- script.js
+++ script.js
@@ -10,35 +10,43 @@ const roundResults = document.querySelector("#game-updates");
 const playerTotal = document.querySelector("#player-total");
 const computerTotal = document.querySelector("#computer-total");
 
-const buttons = Array.from(document.querySelectorAll(".button"));
+const buttons = Array.from(document.querySelectorAll("button"));
 
-function computerSelector() 
-{
+function computerSelector() {
     let index = Math.floor(Math.random() * computerOptions.length)
     return computerOptions[index];
 }
 
-buttons.forEach( (button) => 
-{
-    button.addEventListener("click", () => 
-    {
-        playerChoice = button.id;
-        computerChoice = computerSelector();
-        singleRound(playerChoice, computerChoice);
-    })
+buttons.forEach( (button) => {
+    button.addEventListener("click", clickBtn)
 })
+
+function clickBtn(e) 
+{
+    playerChoice = e.target.id;
+    computerChoice = computerSelector();
+    singleRound(playerChoice, computerChoice);
+}
+
+function gameOver() 
+{
+    buttons.forEach( (button) => 
+    {
+        button.removeEventListener("click", clickBtn);
+    })
+}
 
 function singleRound(playerChoice, computerChoice)
 {
-    if ((playerChoice == "rock" && computerChoice == "scissors") 
-            || (playerChoice == "scissors" && computerChoice == "paper")
-            || (playerChoice == "paper" && computerChoice == "rock"))
+    if ((playerChoice === "rock" && computerChoice === "scissors") 
+            || (playerChoice === "scissors" && computerChoice === "paper")
+            || (playerChoice === "paper" && computerChoice === "rock"))
     {
         
         roundResults.textContent = `You win! ${playerChoice} beats ${computerChoice}`;
         playerScore++;
     } 
-    else if (playerChoice == computerChoice)
+    else if (playerChoice === computerChoice)
     {
         roundResults.textContent = `Draw both players chose ${playerChoice}`
     } 
@@ -51,13 +59,14 @@ function singleRound(playerChoice, computerChoice)
     playerTotal.textContent = `Player score: ${playerScore}`;
     computerTotal.textContent = `Computer score: ${computerScore}`;
 
-    if (playerScore == WINNING_SCORE)
+    if (playerScore === WINNING_SCORE)
     {
         roundResults.textContent = "You win";
+        gameOver();
     } 
-    else if (computerScore == WINNING_SCORE)
+    else if (computerScore === WINNING_SCORE)
     {
         roundResults.textContent = "You lose";
+        gameOver();
     }
 }
-
